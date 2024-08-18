a1b40dda0afea7ddd2f2df84e77ea61542e1698f
Cypress <kad2321@yahoo.com>
2024-08-18 15:42:06 -0400

    Working on exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Reading is Fundamental";
+   let book1 = "https://prodimage.images-bn.com/pimages/9780060853976_p0_v12_s1200x630.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/81UgIZXiolL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/81thdg0KmZL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Good Omens by Neil Gaiman and Terry Prachett" />
+         <img src={book2} alt="Gideon the Ninth by Tamsyn Muir" />
+         <img src={book3} alt="All Systems Read by Martha Wells" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,20 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from 'react';
+import classes from './ChoresList.module.css';
+
+class ChoresList extends React.Component {
+   render() {
+      const chores = ["Do Dishes", "Clean Floors", "Fight a Dragon"];
+      return (
+         <div>
+            <h2 className = {classes.choresHeading}>Chores List</h2>
+               <ol>
+                  <li className = {classes.choresText}>{chores[0]}</li>
+                  <li className = {classes.choresText}>{chores[1]}</li>
+                  <li className = {classes.choresText}>{chores[2]}</li>
+               </ol>
+         </div>
+      );
+   }
 }
+
+export default ChoresList;
